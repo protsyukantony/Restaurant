a433cdeb83c7945d0c925001de8062284fd484b4
Anton Protsyuk <protsyukantony@yandex.ru>
2021-03-16 10:45:55 +0300

    Added a slider, adapted for a tablet

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 const swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 1
-  });
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  loop: true,
+  autoplay: true,
+});
+
+
+$(function () {
+  $('.header-burger').on('click', function () {
+    $('.header-burger,.menu').toggleClass('active')
+  })
+})
